0736e08cdc576d5cb3691e665ef2ac7dc63a2b98
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-03-11 10:24:19 -0700

    table component examples

--- benchmark.rb
+++ benchmark.rb
@@ -4,11 +4,16 @@ require './renderers.rb'
 
 Benchmark.ips do |x|
   x.time = 10
-  x.warmup = 10
+  x.warmup = 2
 
-
-  Renderers.recursive_renderers.each do |renderer, render_function|
-    x.report(renderer.to_s) { render_function.call() }
+  if ENV["TYPE"] == "cables"
+    Renderers.cable_renderers.each do |renderer, render_function|
+      x.report(renderer.to_s) { render_function.call() }
+    end
+  else
+    Renderers.recursive_renderers.each do |renderer, render_function|
+      x.report(renderer.to_s) { render_function.call() }
+    end
   end
 
   x.compare!
